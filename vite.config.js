3e804d7e5dbe0a876f3ecbdda60267bdb5c0cc6c
Manu Kumar Singh <114896741+manu2565@users.noreply.github.com>
2025-04-02 19:21:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1024, // Increase limit to 1MB (default is 500KB)
     outDir: "dist",
     assetsDir: "assets",
     manifest: true
-  },
-  base: "./"
+  }
 });
